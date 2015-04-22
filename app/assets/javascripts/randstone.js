b273dd26745cb23b7b4de8af3096cecf0a9b7eed
Vorgnr <vorgnr@hotmail.fr>
2015-04-22 15:19:52 +0200

    User collection add and remove one

--- app/assets/javascripts/randstone.js
+++ app/assets/javascripts/randstone.js
@@ -9,17 +9,11 @@ var Randstone = {
         pagination: {
             offset: 0,
             limit: 20,
-            initialLineNumber: 4,
-            getCards: function(url, offset, limit, callback) {
-                Randstone.ajax(url, {
-                    offset: offset,
-                    limit: limit
-                }, callback);
-            }
+            initialLineNumber: 4
         }
     },
-    ajax: function(url, data, success, error) {
-        $.ajax({
+    ajax: function(url, data) {
+        return $.ajax({
             type: 'post',
             url: url,
             data: JSON.stringify(data),
@@ -27,8 +21,7 @@ var Randstone = {
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json"
-            },
-            success: success
+            }
         });
     },
     delay: (function(){
--- app/views/users/get_cards.json.jbuilder
+++ app/views/users/get_cards.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@cards) do |card|
-  json.extract! card, :id, :image, :total, :name
+  json.extract! card, :id, :image, :total, :name, :quality
   json.image image_path(medium_card_image_path(card.image))
 end
