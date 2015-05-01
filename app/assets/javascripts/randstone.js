4101acad5cd738e3868ad56dce00d86a272512a9
vorgnr <arnaud.cavat@gmail.com>
2015-05-01 14:15:48 -0700

    Improve Ranstone global js

--- app/assets/javascripts/randstone.js
+++ app/assets/javascripts/randstone.js
@@ -1,34 +1,40 @@
 
-var Randstone = {
-    image: {
-        medium: {
-            height: 270
-        }
-    },
-    cardCollections: {
-        pagination: {
-            offset: 0,
-            limit: 20,
-            initialLineNumber: 4
-        }
-    },
-    ajax: function(url, data) {
-        return $.ajax({
-            type: 'post',
-            url: url,
-            data: JSON.stringify(data),
-            dataType: "json",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            }
-        });
-    },
-    delay: (function(){
+function Randstone() {
+    this.config = {
+       image: {
+           medium: {
+               height: 270
+           }
+       },
+       cardCollections: {
+           pagination: {
+               offset: 0,
+               limit: 20,
+               initialLineNumber: 4
+           }
+       } 
+    };
+
+    this.delay = (function(){
         var timer = 0;
         return function(callback, ms){
             clearTimeout (timer);
             timer = setTimeout(callback, ms);
         };
-    })()
+    })();
+}
+
+Randstone.prototype.ajax = function(url, data) {
+    return $.ajax({
+        type: 'post',
+        url: url,
+        data: JSON.stringify(data),
+        dataType: "json",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    });
 };
+
+window.Randstone = new Randstone();
